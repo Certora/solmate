e3a64d456e260b21b435fe818b39cc1714c066d8
Thomas Adams <thomas.harlan.adams@gmail.com>
2022-09-08 07:57:20 -0700

    Added run script for ERC20.sol

---
+++ certora/scripts/verifyERC20.sh
@@ -0,0 +1,15 @@
+if [[ "$1" ]]
+then
+    RULE="--rule $1"
+fi
+
+certoraRun \
+    certora/harnesses/tokens/ERC20Harness.sol \
+    --verify ERC20Harness:certora/specs/ERC20.spec \
+    --solc solc8.0 \
+    --optimistic_loop \
+    --loop_iter 3 \
+    --send_only \
+    --cloud \
+    $RULE \
+    --msg "ERC20 verification: $R1"
