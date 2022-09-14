d97fa6d2114dc3ce9437b8409eca2ad03f43ee68
Thomas Adams <thomas.harlan.adams@gmail.com>
2022-09-14 10:32:04 -0700

    Modified two auth files to conform to naming conventions

---
+++ certora/harnesses/auth/OwnedHarness.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0;
+
+import {Owned} from "../../../src/auth/Owned.sol";
+
+/// @notice A harness implementation for an (empty) owned contract
+/// @dev The first owner of this contract is the deployer
+contract OwnedHarness is Owned {
+    constructor() Owned(msg.sender) {}
+}
